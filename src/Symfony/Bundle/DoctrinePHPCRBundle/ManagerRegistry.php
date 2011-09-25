54e97e63a0004e092be9467c015c946294785eed
Lukas Kahwe Smith <smith@pooteeweet.org>
2011-09-25 23:27:24 +0200

    added ManagerRegistry

---
+++ src/Symfony/Bundle/DoctrinePHPCRBundle/ManagerRegistry.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Symfony\Bundle\DoctrinePHPCRBundle;
+
+use Symfony\Bridge\Doctrine\ManagerRegistry as BaseManagerRegistry;
+use Doctrine\ODM\PHPCR\PHPCRException;
+
+class ManagerRegistry extends BaseManagerRegistry
+{
+    /**
+     * Resolves a registered namespace alias to the full namespace.
+     *
+     * @param string $alias
+     * @return string
+     * @throws PHPCRException
+     */
+    public function getAliasNamespace($alias)
+    {
+        foreach (array_keys($this->getManagers()) as $name) {
+            try {
+                return $this->getManager($name)->getConfiguration()->getDocumentNamespace($alias);
+            } catch (PHPCRException $e) {
+            }
+        }
+
+        throw PHPCRException::unknownEntityNamespace($alias);
+    }
+}
