30af26d110d9686292e6b81a3772031ddbb01473
Lukas Kahwe Smith <smith@pooteeweet.org>
2011-10-17 23:09:46 +0200

    removed unused use statement

--- src/Symfony/Cmf/Bundle/MultilangContentBundle/SymfonyCmfMultilangContentBundle.php
+++ src/Symfony/Cmf/Bundle/MultilangContentBundle/SymfonyCmfMultilangContentBundle.php
@@ -5,8 +5,6 @@ namespace Symfony\Cmf\Bundle\MultilangContentBundle;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 
-use Liipch\TranslationBundle\DependencyInjection\Compiler\EventRegistrarPass;
-
 /**
  * @author brian.king (at) liip.ch
  */
