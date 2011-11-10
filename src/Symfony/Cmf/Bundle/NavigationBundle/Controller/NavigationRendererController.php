4b1930cf1a696b32c533cea88611946b22c885b1
Roland Schilter <roland.schilter@liip.ch>
2011-11-10 13:52:20 +0100

    consistent naming of menu param

--- src/Symfony/Cmf/Bundle/NavigationBundle/Controller/NavigationRendererController.php
+++ src/Symfony/Cmf/Bundle/NavigationBundle/Controller/NavigationRendererController.php
@@ -98,7 +98,7 @@ class NavigationRendererController extends Controller
     {
         $map = $this->walker->getMenu('/', true, -1);
         return $this->render('SymfonyCmfNavigationBundle:NavigationRenderer:sitemap.html.twig',
-                             array('map' => $map,
+                             array('root' => $map,
                                    'routename' => $this->routename));
     }
 }
