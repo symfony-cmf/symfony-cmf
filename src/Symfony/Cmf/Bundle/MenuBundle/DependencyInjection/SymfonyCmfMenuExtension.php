4e494e5b4b13c6a4e0d22561a6592c68db88d6e9
dbu <david.buchmann@liip.ch>
2011-12-20 13:23:01 +0100

    apply Stof's comment on DI to MenuBundle as well

--- src/Symfony/Cmf/Bundle/MenuBundle/DependencyInjection/SymfonyCmfMenuExtension.php
+++ src/Symfony/Cmf/Bundle/MenuBundle/DependencyInjection/SymfonyCmfMenuExtension.php
@@ -11,14 +11,12 @@ class SymfonyCmfMenuExtension extends Extension
 {
     public function load(array $configs, ContainerBuilder $container)
     {
-        $processor = new Processor();
-        $configuration = new Configuration();
-        $config = $processor->processConfiguration($configuration, $configs);
-
-        $container->setParameter('symfony_cmf_menu.menu_root', $config['menu_root']);
-        $container->setParameter('symfony_cmf_menu.document_manager', $config['document_manager']);
+        $config = $this->processConfiguration(new Configuration(), $configs);
 
         $loader = new XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('phpcr-menu.xml');
+
+        $container->setParameter('symfony_cmf_menu.menu_root', $config['menu_root']);
+        $container->setParameter('symfony_cmf_menu.document_manager', $config['document_manager']);
     }
 }
