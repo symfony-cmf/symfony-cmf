106020057873446ef3d60fb2187b32886e4f7e40
dbu <david.buchmann@liip.ch>
2011-12-20 16:11:35 +0100

    provide default for menu_basepath

--- src/Symfony/Cmf/Bundle/MenuBundle/DependencyInjection/Configuration.php
+++ src/Symfony/Cmf/Bundle/MenuBundle/DependencyInjection/Configuration.php
@@ -14,7 +14,7 @@ class Configuration implements ConfigurationInterface
 
         $rootNode
             ->children()
-                ->scalarNode('menu_basepath')->end()
+                ->scalarNode('menu_basepath')->defaultValue('/cms/menu')->end()
                 ->scalarNode('document_manager')->defaultValue('doctrine_phpcr.odm.default_document_manager')->end()
             ->end()
         ;
