a53d9b8e86ca3c25a7c341f55c07e6d9405a84d8
Jacopo Jakuza Romei <jromei@gmail.com>
2012-01-02 18:05:53 +0100

    Making PHPCRBrowserBundle more decoupled by its context.

---
+++ src/Symfony/Cmf/Bundle/PHPCRBrowserBundle/Resources/public/js/init.js
@@ -0,0 +1,60 @@
+function initTree(config) {
+
+    jQuery(config.selector).jstree({ 
+        "core": { 
+            "initially_load": config.path.expanded,
+            "initially_open": config.path.preloaded
+        },
+        "plugins": [ "contextmenu", "themes", "types", "ui", "json_data" ],
+        "json_data": {
+            "ajax": {
+                url:    Routing.generate('symfony_cmf_phpcr_browser_children'),
+                data:   function (node) {
+                    return { 'root' : jQuery(node).attr('id') };
+                }
+            }
+        },
+        "types": {
+            "max_depth":        -2,
+            "max_children":     -2,
+            "valid_children":  [ "folder" ],
+            "types": {
+                "default": {
+                    "valid_children": "none",
+                    "icon": {
+                        "image": config.icon.document
+                    }
+                },
+                "folder": {
+                    "valid_children": [ "default", "folder" ],
+                    "icon": {
+                        "image": config.icon.folder
+                    }
+                }
+            }
+        },
+        "contextmenu": {
+            "items": {
+                "rename":   null,
+                "remove":   null,
+                "ccp":      null,
+                "create": {
+                    "label":    "Create",
+                    "action":   function (node) {
+                                    window.location = Routing.generate(config.route.create, { "id": node.attr("id") })
+                                }
+                },
+                "delete": {
+                    "label":    "Delete",
+                    "action":   function (node) {
+                                    window.location = Routing.generate(config.route.erase, { "id": node.attr("id") })
+                                }
+                }
+            }
+        }
+    })
+    .bind("select_node.jstree", function (event, data) {
+        window.location = Routing.generate(config.route.edit, { "id": data.rslt.obj.attr("id") });
+    })
+    .delegate("a", "click", function (event, data) { event.preventDefault(); });
+}
