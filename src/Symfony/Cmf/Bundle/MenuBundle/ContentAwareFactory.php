81e3a7900503e860750abf5d77915fd6b3040c6b
lsmith77 <smith@pooteeweet.org>
2012-01-10 08:20:58 +0100

    type hint the interface

--- src/Symfony/Cmf/Bundle/MenuBundle/ContentAwareFactory.php
+++ src/Symfony/Cmf/Bundle/MenuBundle/ContentAwareFactory.php
@@ -6,7 +6,7 @@ use Knp\Menu\Silex\RouterAwareFactory;
 
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
-use Symfony\Component\DependencyInjection\Container;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 use Symfony\Cmf\Bundle\ChainRoutingBundle\Routing\DoctrineRouter;
 
@@ -20,7 +20,7 @@ class ContentAwareFactory extends RouterAwareFactory
      * @param DoctrineRouter $contentRouter to generate routes when content is set
      * @param Container $container to fetch the request in order to determine whether this is the current menu item
      */
-    public function __construct(UrlGeneratorInterface $generator, DoctrineRouter $contentRouter, Container $container)
+    public function __construct(UrlGeneratorInterface $generator, DoctrineRouter $contentRouter, ContainerInterface $container)
     {
         parent::__construct($generator);
         $this->contentRouter = $contentRouter;
