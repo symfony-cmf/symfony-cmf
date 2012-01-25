b7fa6fbf472f4a9d55198187aef9ee3e9b2b02c0
dbu <david.buchmann@liip.ch>
2012-01-25 14:09:56 +0100

    making the cmf menu bundle less dependent on other cmf components. see #133

--- src/Symfony/Cmf/Bundle/MenuBundle/DependencyInjection/SymfonyCmfMenuExtension.php
+++ src/Symfony/Cmf/Bundle/MenuBundle/DependencyInjection/SymfonyCmfMenuExtension.php
@@ -1,11 +1,15 @@
 <?php
 namespace Symfony\Cmf\Bundle\MenuBundle\DependencyInjection;
 
-use Symfony\Component\HttpKernel\DependencyInjection\Extension;
-use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\Config\Definition\Processor;
-use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
+
 use Symfony\Component\Config\FileLocator;
+use Symfony\Component\Config\Definition\Processor;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
+use Symfony\Component\DependencyInjection\Reference;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+
+use Symfony\Cmf\Bundle\ChainRoutingBundle\Routing\DoctrineRouter;
 
 class SymfonyCmfMenuExtension extends Extension
 {
@@ -16,7 +20,17 @@ class SymfonyCmfMenuExtension extends Extension
         $loader = new XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('phpcr-menu.xml');
 
-        $container->setParameter('symfony_cmf_menu.menu_basepath', $config['menu_basepath']);
-        $container->setParameter('symfony_cmf_menu.document_manager', $config['document_manager']);
+        $container->setParameter($this->getAlias() . '.menu_basepath', $config['menu_basepath']);
+        $container->setParameter($this->getAlias() . '.document_manager', $config['document_manager']);
+        $container->setParameter($this->getAlias() . '.menu_document_class', $config['menu_document_class']);
+
+        $factory = $container->getDefinition($this->getAlias().'.factory');
+        $factory->replaceArgument(2, new Reference($config['content_url_generator']));
+        $container->setParameter($this->getAlias() . '.content_key', $config['content_key']);
+        if (empty($config['content_key'])) {
+            $config['content_key'] = DoctrineRouter::CONTENT_KEY;
+        }
+        $container->setParameter($this->getAlias() . '.content_key', $config['content_key']);
+        $container->setParameter($this->getAlias() . '.route_name', $config['route_name']);
     }
 }
