b945734516dac2b97ad9db56ba3fb683cf7886c9
lsmith77 <smith@pooteeweet.org>
2012-05-05 15:17:25 +0200

    moved code to separate repos

--- src/Symfony/Cmf/Bundle/ContentBundle/SymfonyCmfContentBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Symfony\Cmf\Bundle\ContentBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class SymfonyCmfContentBundle extends Bundle
-{
-}
--- src/Symfony/Cmf/Bundle/PHPCRBrowserBundle/SymfonyCmfPHPCRBrowserBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Symfony\Cmf\Bundle\PHPCRBrowserBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class SymfonyCmfPHPCRBrowserBundle extends Bundle
-{
-}
